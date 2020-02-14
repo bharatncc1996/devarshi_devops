7edc40231d352ca48f2739c8107b5232c8a4686a
bharatncc1996 <46753667+bharatncc1996@users.noreply.github.com>
2020-02-14 14:57:29 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+print('done')
 os.system('touch {}'.format(sys.argv[1]))
 print('hello {} good eveng how r u your file {} was created successfully '.format(sys.argv[1],sys.argv[2]))
 
